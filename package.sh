1669502637f77b0d5a99a63f861b2a152d4c6e64
Rebecca Taylor <bex@beccasaur.us>
2020-08-01 17:06:53 -0700

    Getting ready for a new world.

--- package.sh
+++ package.sh
@@ -1,7 +1,7 @@
-name @import
+name import
 
 description "This is the heart of everything."
 
 version "$( grep IMPORT_VERSION= import.sh | sed "s/.*\([0-9]\.[0-9]\.[0-9]\).*/\1/" )"
 
-exclude spec
+exclude spec/
